4797bdc143f0851aa502848b30820f7ebcf448f9
/snowep <42274762+snowep@users.noreply.github.com>
2022-09-09 16:34:16 +0800

    config

--- app/config/config.php
+++ app/config/config.php
@@ -4,6 +4,6 @@ define('BASEURL', 'https://9c5d-103-144-132-4.ap.ngrok.io/cammelia-project/publi
 define('BASE_FILE_DIR', 'https://9c5d-103-144-132-4.ap.ngrok.io/cammelia-project/public/uploads/reports/');
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'cammelia');
+define('DB_USER', 'n1605519');
+define('DB_PASS', 'aasdasd123');
+define('DB_NAME', 'n1605519_cammelia');
